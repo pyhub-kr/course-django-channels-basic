1b9cb3f5f9594e649ca5ec9708d97cd33b3fdca7
Chinseok Lee <me@pyhub.kr>
2022-12-02 16:46:06 +0900

    JS 객체 웹소켓 전송 및 수신

--- app/consumers.py
+++ app/consumers.py
@@ -1,7 +1,16 @@
+import json
+
 from channels.generic.websocket import WebsocketConsumer
 
 
 class EchoConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
-        self.send(f"You said : {text_data}")
+        obj = json.loads(text_data)
+        print("수신 :", obj)
+
+        json_string = json.dumps({
+            "content": obj["content"],
+            "user": obj["user"],
+        })
+        self.send(json_string)
