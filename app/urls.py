11c9340153426bf0a329c491a5b0d34e212da591
Chinseok Lee <me@pyhub.kr>
2022-12-02 16:23:14 +0900

    초간단 Echo 웹소켓 구현

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from app import views
 
-urlpatterns = []
+urlpatterns = [
+    path("echo/", views.echo_page),
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def echo_page(request):
+    return render(request, "app/echo_page.html")
