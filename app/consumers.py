b16e76fb91148ec60844f17ff4555d8927a95479
Chinseok Lee <me@pyhub.kr>
2022-12-05 22:47:18 +0900

    WebsocketConsumer를 JsonWebsocketConsumer로 리팩토링

--- app/consumers.py
+++ app/consumers.py
@@ -1,29 +1,27 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class LiveblogConsumer(WebsocketConsumer):
+class LiveblogConsumer(JsonWebsocketConsumer):
     groups = ["liveblog"]
 
     def liveblog_post_created(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(event_dict)
 
     def liveblog_post_updated(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(event_dict)
 
     def liveblog_post_deleted(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(event_dict)
 
 
-class EchoConsumer(WebsocketConsumer):
+class EchoConsumer(JsonWebsocketConsumer):
 
-    def receive(self, text_data=None, bytes_data=None):
-        obj = json.loads(text_data)
-        print("수신 :", obj)
+    def receive_json(self, content, **kwargs):
+        print("수신 :", content)
 
-        json_string = json.dumps({
-            "content": obj["content"],
-            "user": obj["user"],
+        self.send_json({
+            "content": content["content"],
+            "user": content["user"],
         })
-        self.send(json_string)
