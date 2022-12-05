adaf4c28a959cb27135aa9247e6f28015431cac1
Chinseok Lee <me@pyhub.kr>
2022-12-05 16:30:40 +0900

    채널레이어, 레디스 접속 테스트

---
+++ run_test_hello_channel.py
@@ -0,0 +1,22 @@
+import asyncio
+import os
+
+import django
+from channels.layers import get_channel_layer
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
+django.setup()
+
+
+async def main():
+    channel_layer = get_channel_layer()
+
+    message_dict = {'content': 'world'}
+
+    await channel_layer.send('hello', message_dict)
+    response_dict = await channel_layer.receive('hello')
+    is_equal = message_dict == response_dict
+    print("송신/수신 데이터가 같습니까?", is_equal)
+
+
+asyncio.run(main())
