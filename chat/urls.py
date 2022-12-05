244522d2e213f6560be2413e089ab27654e61aa2
Chinseok Lee <me@pyhub.kr>
2022-12-05 23:10:04 +0900

    chat room_chat 뷰 구현

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ app_name = "chat"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:room_name>/chat/", views.room_chat, name="room_chat"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "chat/index.html")
+
+
+def room_chat(request, room_name):
+    return render(request, "chat/room_chat.html", {
+        "room_name": room_name,
+    })
