4fe89c582867e30abb039e6d911cc4a1842e0ca5
Chinseok Lee <me@pyhub.kr>
2022-12-05 13:20:30 +0900

    LiveblogConsumer 구현

--- app/consumers.py
+++ app/consumers.py
@@ -3,6 +3,19 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 
+class LiveblogConsumer(WebsocketConsumer):
+    groups = ["liveblog"]
+
+    def liveblog_post_created(self, event_dict):
+        self.send(json.dumps(event_dict))
+
+    def liveblog_post_updated(self, event_dict):
+        self.send(json.dumps(event_dict))
+
+    def liveblog_post_deleted(self, event_dict):
+        self.send(json.dumps(event_dict))
+
+
 class EchoConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
--- app/routing.py
+++ app/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.consumers import EchoConsumer
+from app.consumers import EchoConsumer, LiveblogConsumer
 
 websocket_urlpatterns = [
+    path("ws/liveblog/", LiveblogConsumer.as_asgi()),
     path("ws/echo/", EchoConsumer.as_asgi()),
 ]
