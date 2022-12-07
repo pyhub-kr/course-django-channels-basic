6bba9a458d9867568f6d4439f8c1b53365020fdb
Chinseok Lee <me@pyhub.kr>
2022-12-07 17:39:10 +0900

    채팅방 별로 다른 채팅레이어 그룹

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,8 +3,27 @@ from channels.generic.websocket import JsonWebsocketConsumer
 
 
 class ChatConsumer(JsonWebsocketConsumer):
-    SQUARE_GROUP_NAME = "square"
-    groups = [SQUARE_GROUP_NAME]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.group_name = ""
+
+    def connect(self):
+        room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.group_name = f"chat-{room_name}"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            self.channel_name,
+        )
+
+        self.accept()
+
+    def disconnect(self, code):
+        if self.group_name:
+            async_to_sync(self.channel_layer.group_discard)(
+                self.group_name,
+                self.channel_name,
+            )
 
     def receive_json(self, content, **kwargs):
         _type = content["type"]
@@ -12,7 +31,7 @@ class ChatConsumer(JsonWebsocketConsumer):
         if _type == "chat.message":
             message = content["message"]
             async_to_sync(self.channel_layer.group_send)(
-                self.SQUARE_GROUP_NAME,
+                self.group_name,
                 {
                     "type": "chat.message",
                     "message": message,
