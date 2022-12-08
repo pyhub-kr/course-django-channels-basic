d18355a497743d57a3504a0f9c136fa1d9b35b9a
Chinseok Lee <me@pyhub.kr>
2022-12-08 16:10:32 +0900

    채팅방 참여자 목록 - 채팅방 입장/퇴장 실시간 이벤트 처리

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,6 +8,7 @@ class ChatConsumer(JsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.group_name = ""
+        self.room = None
 
     def connect(self):
         user = self.scope["user"]
@@ -16,14 +17,30 @@ class ChatConsumer(JsonWebsocketConsumer):
             self.close()
         else:
             room_pk = self.scope["url_route"]["kwargs"]["room_pk"]
-            self.group_name = Room.make_chat_group_name(room_pk=room_pk)
 
-            async_to_sync(self.channel_layer.group_add)(
-                self.group_name,
-                self.channel_name,
-            )
+            try:
+                self.room = Room.objects.get(pk=room_pk)
+            except Room.DoesNotExist:
+                self.close()
+            else:
+                self.group_name = self.room.chat_group_name
 
-            self.accept()
+                is_new_join = self.room.user_join(self.channel_name, user)
+                if is_new_join:
+                    async_to_sync(self.channel_layer.group_send)(
+                        self.group_name,
+                        {
+                            "type": "chat.user.join",
+                            "username": user.username,
+                        }
+                    )
+
+                async_to_sync(self.channel_layer.group_add)(
+                    self.group_name,
+                    self.channel_name,
+                )
+
+                self.accept()
 
     def disconnect(self, code):
         if self.group_name:
@@ -31,6 +48,19 @@ class ChatConsumer(JsonWebsocketConsumer):
                 self.group_name,
                 self.channel_name,
             )
+
+        user = self.scope["user"]
+
+        if self.room is not None:
+            is_last_leave = self.room.user_leave(self.channel_name, user)
+            if is_last_leave:
+                async_to_sync(self.channel_layer.group_send)(
+                    self.group_name,
+                    {
+                        "type": "chat.user.leave",
+                        "username": user.username,
+                    }
+                )
 
     def receive_json(self, content, **kwargs):
         user = self.scope["user"]
@@ -50,6 +80,18 @@ class ChatConsumer(JsonWebsocketConsumer):
             )
         else:
             print(f"Invalid message type : ${_type}")
+
+    def chat_user_join(self, message_dict):
+        self.send_json({
+            "type": "chat.user.join",
+            "username": message_dict["username"],
+        })
+
+    def chat_user_leave(self, message_dict):
+        self.send_json({
+            "type": "chat.user.leave",
+            "username": message_dict["username"],
+        })
 
     def chat_message(self, message_dict):
         self.send_json({
