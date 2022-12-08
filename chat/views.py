12e6430d44570ae6948b5ffc7222773d716d6ea8
Chinseok Lee <me@pyhub.kr>
2022-12-08 13:17:05 +0900

    채팅방 페이지에서 인증 체크하기

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
 from chat.forms import RoomForm
@@ -11,6 +12,7 @@ def index(request):
     })
 
 
+@login_required
 def room_new(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
@@ -25,6 +27,7 @@ def room_new(request):
     })
 
 
+@login_required
 def room_chat(request, room_pk):
     room = get_object_or_404(Room, pk=room_pk)
     return render(request, "chat/room_chat.html", {
