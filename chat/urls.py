79e620f93ceddc7309dd0eb3365941f18d89abad
Chinseok Lee <me@pyhub.kr>
2022-12-08 13:45:16 +0900

    채팅방 삭제 - room_delete 뷰 구현

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("new/", views.room_new, name="room_new"),
     path("<str:room_pk>/chat/", views.room_chat, name="room_chat"),
+    path("<str:room_pk>/delete/", views.room_delete, name="room_delete"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -33,5 +34,23 @@ def room_new(request):
 def room_chat(request, room_pk):
     room = get_object_or_404(Room, pk=room_pk)
     return render(request, "chat/room_chat.html", {
+        "room": room,
+    })
+
+
+@login_required
+def room_delete(request, room_pk):
+    room = get_object_or_404(Room, pk=room_pk)
+
+    if room.owner != request.user:
+        messages.error(request, "채팅방 소유자가 아닙니다.")
+        return redirect("chat:index")
+
+    if request.method == "POST":
+        room.delete()
+        messages.success(request, "채팅방을 삭제했습니다.")
+        return redirect("chat:index")
+
+    return render(request, "chat/room_confirm_delete.html", {
         "room": room,
     })
