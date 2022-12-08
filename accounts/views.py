a1e8823501c0fcb844359a3b13068e1181b8aed6
Chinseok Lee <me@pyhub.kr>
2022-12-08 13:16:35 +0900

    기본인증 - 회원가입 뷰 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+
+signup = CreateView.as_view(
+    form_class=UserCreationForm,
+    template_name="partials/form.html",
+    extra_context={
+        "form_name": "회원가입",
+        "submit_label": "회원가입",
+    },
+    success_url=reverse_lazy("accounts:login"),
+)
+
 
 login = LoginView.as_view(
     template_name="partials/form.html",
