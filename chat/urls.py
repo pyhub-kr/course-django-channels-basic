43de5de0e2bec7d2debd8880998ac00277bf4a67
Chinseok Lee <me@pyhub.kr>
2022-12-09 21:31:11 +0900

    채팅방 유저목록 JSON API 구현

--- chat/urls.py
+++ chat/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("new/", views.room_new, name="room_new"),
     path("<str:room_pk>/chat/", views.room_chat, name="room_chat"),
     path("<str:room_pk>/delete/", views.room_delete, name="room_delete"),
+    path("<int:room_pk>/users/", views.room_users, name="room_users"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from chat.forms import RoomForm
@@ -53,4 +54,18 @@ def room_delete(request, room_pk):
 
     return render(request, "chat/room_confirm_delete.html", {
         "room": room,
+    })
+
+
+@login_required
+def room_users(request, room_pk):
+    room = get_object_or_404(Room, pk=room_pk)
+
+    if not room.is_joined_user(request.user):
+        return HttpResponse("Unauthorized user", status=401)
+
+    username_list = room.get_online_usernames()
+
+    return JsonResponse({
+        "username_list": username_list,
     })
